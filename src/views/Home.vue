6a90798589386f3f8dafa8688deeecfa2e9a7707
Your Name <you@example.com>
2020-11-13 19:45:52 +0800

    vuerecord

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div>
+      <input type="text" @input="inputchange" v-model="inputdata" />
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import _ from "lodash";
 export default defineComponent({
   name: "Home",
   components: {
     HelloWorld
+  },
+  setup() {
+    const inputdata = ref("");
+    const inputchange = _.debounce(function() {
+      console.log("value", inputdata.value);
+    }, 1000);
+    return {
+      inputchange,
+      inputdata
+    };
   }
 });
 </script>
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {	
+	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+	devServer: {
+	proxy: {        
+		  '/': {    
+		    target: 'https://interface.music.163.com',
+		  }
+		}
+	},
+	chainWebpack: (config) => {
+		config.module
+			.rule("eslint")
+			.use("eslint-loader")
+			.loader("eslint-loader")
+			.tap(options => {
+				options.fix = true; //设置自动修复eslint
+				return options;
+			})
+	}
+}
